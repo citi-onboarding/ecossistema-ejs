fc0f63e2429a569d99165a4d5a09425537907cf0
Júlia Dias <juliia.dias00@gmail.com>
2021-11-30 23:57:30 -0300

    feat(feature/react-banner):Basic structure

---
+++ client/src/Views/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className="banner">
+        <div>
+            <h1>Onde tudo se conecta e você evolui</h1>
+            <p>“Potencializamos a transformação tecnológica através de conexões e integração entre as pessoas, co-criando um aibnente igualitário e inclusivo”.</p>
+        </div>
+    </div>
+  );
+}
+
+export default Banner;
