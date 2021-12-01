c8c0fd0f6cfef03660f7d130b4da157830dd8dba
Júlia Dias <juliia.dias00@gmail.com>
2021-11-30 23:58:47 -0300

    feat(feature/react-banner):Style, banner Image, font family

--- client/src/Views/Banner/Banner.jsx
+++ client/src/Views/Banner/Banner.jsx
@@ -4,9 +4,12 @@ import './Banner.css'
 function Banner() {
   return (
     <div className="banner">
-        <div>
+        <div className="banner-text">
             <h1>Onde tudo se conecta e você evolui</h1>
             <p>“Potencializamos a transformação tecnológica através de conexões e integração entre as pessoas, co-criando um aibnente igualitário e inclusivo”.</p>
+        </div>
+        <div className="banner-image">
+            <div className="image"></div>
         </div>
     </div>
   );
