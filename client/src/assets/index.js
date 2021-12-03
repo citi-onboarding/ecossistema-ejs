46d9d4cae11a66d42dbbc6cbeada3436b23b463e
JoaoSouza9 <joaovictor.souza98@yahoo.com.br>
2021-12-03 18:14:21 -0300

    feat(react-footer-socialmedia): site responsiveness adjusted

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,7 @@
 import Logo from './logo.png';
 import Youtube from './youtube.svg';
 import Google from './google.svg';
-import Logoeco from './logoecoo.svg';
+import Logoeco from './logoeco.svg';
 import Instagram from './instagram.svg';
 
 export {
