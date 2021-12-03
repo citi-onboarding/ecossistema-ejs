b05811a143a9bf25a80d74cf3f591dd9b725202d
mariabdma <mariabma76@gmail.com>
2021-12-03 14:30:46 -0300

    feat(partners-and-aboutus):about us, partners, title component

--- client/src/Views/AboutUs/AboutUs.jsx
+++ client/src/Views/AboutUs/AboutUs.jsx
@@ -5,7 +5,7 @@ import aboutus from '../../assets/aboutus.svg'
 function AboutUs() {
   return (
     <div className="AboutUs">
-     <img src={aboutus} alt='Sobre nós'/>      
+     <img src={aboutus} alt="Sobre nós" widht="445px" height="450px"/>      
      <h1 className="AboutUsTitle" >Ecossistema Internacional de empresa Juniores</h1>
      <p className="AboutUsp1">Somos guiados pelo propósito e pelo sonho de um estado de São Paulo mais empreendedor. Por isso, temos consciência da nossa responsabilidade para fazer a diferença na vida de pequenas, médias e grandes empresas.</p>
      <p className="AboutUsp2">Somos guiados pelo propósito e pelo sonho de um estado de São Paulo mais empreendedor. Por isso, temos consciência da nossa responsabilidade para fazer a diferença na vida de pequenas, médias e grandes empresas.</p>
