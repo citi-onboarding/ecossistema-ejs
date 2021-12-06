2b8aa99a4c50902e3a63ab8e47b7a85ee6e389f5
JoaoSouza9 <joaovictor.souza98@yahoo.com.br>
2021-12-06 15:52:35 -0300

    fix(react-footer-socialmedia): site's responsiveness

--- client/src/Views/Contato/Contato.jsx
+++ client/src/Views/Contato/Contato.jsx
@@ -16,31 +16,33 @@ import {
 function Contato() {
     return (
         <div id="contactus">
-            <section id="addres">
-                <div>
-                    <Contacttext justify="left" Contacttext="Onde nos encontrar:"/>
-                    <Contacttext justify="left" Contacttext="Av. Jornalista Aníbal Fernandes, s/n –
-                        Cidade Universitária, Recife – PE,
-                        50740-560"/>
-                </div>
-            </section>
-            <section id="logo">
-                <img src= {Logoeco} alt="" />
-            </section>
-            <section id="contact">
-                <div id="reach_us">
-                    <Contacttext justify="right" Contacttext="Fale conosco!"/>
-                    <Contacttext justify="right" Contacttext="+55 (81) 99429-9649"/>
-                    <div id="email">
-                        <Contacttext justify="right" Contacttext="contato@citi.org.br"/>
-                    </div>
-                </div>
-                <div id="socialmedia">
-                    <SocialMediaButton link="" image= {Instagram} imagedescription=""/>
-                    <SocialMediaButton link="" image= {Youtube} imagedescription=""/>
-                    <SocialMediaButton link="" image= {Google} imagedescription=""/>
-                </div>
-            </section>
+            <div className="contato">
+                <section id="addres">
+                        <div>
+                            <Contacttext justify="left" Contacttext="Onde nos encontrar:"/>
+                            <Contacttext justify="left" Contacttext="Av. Jornalista Aníbal Fernandes, s/n –
+                                Cidade Universitária, Recife – PE,
+                                50740-560"/>
+                        </div>
+                    </section>
+                    <section id="logo">
+                        <img src= {Logoeco} alt="" />
+                    </section>
+                    <section id="contact">
+                        <div id="reach_us">
+                            <Contacttext justify="right" Contacttext="Fale conosco!"/>
+                            <Contacttext justify="right" Contacttext="+55 (81) 99429-9649"/>
+                            <div id="email">
+                                <Contacttext justify="right" Contacttext="contato@citi.org.br"/>
+                            </div>
+                        </div>
+                        <div id="socialmedia">
+                            <SocialMediaButton link="" image= {Instagram} imagedescription=""/>
+                            <SocialMediaButton link="" image= {Youtube} imagedescription=""/>
+                            <SocialMediaButton link="" image= {Google} imagedescription=""/>
+                        </div>
+                    </section>
+            </div>
         </div>
     )
 }
