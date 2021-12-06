c9b52301b2d62933ada7c2efd9bf83f9009be9ea
Júlia Dias <juliia.dias00@gmail.com>
2021-12-06 15:48:29 -0300

    feat(feature/react-banner):Max width

--- client/src/Views/Banner/Banner.jsx
+++ client/src/Views/Banner/Banner.jsx
@@ -9,6 +9,7 @@ import {
 function Banner() {
   return (
     <div className="banner">
+      <div className="max-width">
         <div className="banner-text">
             <h1>Onde tudo se conecta e você evolui</h1>
             <p>“Potencializamos a transformação tecnológica através de conexões e integração entre as pessoas, co-criando um aibnente igualitário e inclusivo”.</p>
@@ -16,6 +17,7 @@ function Banner() {
         </div>
         <div className="banner-image">
             <div className="image"></div>
+        </div>
         </div>
         <Divider/>
     </div>
