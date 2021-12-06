6bb2b259dca38e4329d544025db291b490d44f0e
Júlia Dias <juliia.dias00@gmail.com>
2021-12-06 15:40:55 -0300

    feat(feature/react-banner):Button link

--- client/src/Components/Button/Button.jsx
+++ client/src/Components/Button/Button.jsx
@@ -5,7 +5,9 @@ import './Button.css';
 function Button(props) {
 /* O "style" abaixo é um exemplo de como passar um estilo do css como props  */
   return (
+    <a target="_blank" href={props.link}>
        <button className="button"> {props.text}</button> 
+       </a>
   );
 }
 
--- client/src/Views/Banner/Banner.jsx
+++ client/src/Views/Banner/Banner.jsx
@@ -12,7 +12,7 @@ function Banner() {
         <div className="banner-text">
             <h1>Onde tudo se conecta e você evolui</h1>
             <p>“Potencializamos a transformação tecnológica através de conexões e integração entre as pessoas, co-criando um aibnente igualitário e inclusivo”.</p>
-            <Button text="Saiba mais"/>
+            <Button link="https://www.google.com.br" text="Saiba mais"/>
         </div>
         <div className="banner-image">
             <div className="image"></div>
