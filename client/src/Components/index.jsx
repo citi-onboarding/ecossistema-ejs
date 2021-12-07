ade3c3c05b5a1069dcc18b24ed557a0b792eb325
Júlia Dias <juliia.dias00@gmail.com>
2021-12-07 19:26:57 -0300

    fix(fix/react-design-fixess):Fix component routes

--- client/src/Components/index.jsx
+++ client/src/Components/index.jsx
@@ -1,5 +1,4 @@
 export {default as PartnersComponent} from './PartnersComponent';
 export {default as ExampleComponent} from './ExampleComponent';
-export {default as ExampleComponent} from './ExampleComponent';
 export {default as Button} from './Button';
 export {default as Divider} from './Divider';
