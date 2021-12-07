2b7601ece55ec6a0682b45225b93f820f1de062a
JoaoSouza9 <joaovictor.souza98@yahoo.com.br>
2021-12-07 19:12:38 -0300

    fix(react-footer-socialmedia): adjusted the footer width

--- client/src/Views/Footer/Footer.jsx
+++ client/src/Views/Footer/Footer.jsx
@@ -7,14 +7,17 @@ import {
 
 function Footer(){
   return (
-    <div id="footer">
-        <div id="footer-left">
+    <div className="footer-content">
+        <div id="footer">
+          <div id="footer-left">
             <Footertext footertext="Copyright©2021" justify="left"/>
         </div>
         <div id="footer-right">
             <Footertext footertext="Made with &lt;/&gt; and ❤ by CITI" justify="right"/>
         </div>
+     </div>
     </div>
+
   );
 }
 
