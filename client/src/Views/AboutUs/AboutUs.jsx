a44501d814c18872cd63bd778e5463d8970200e9
mariabdma <mariabma76@gmail.com>
2021-12-09 11:37:11 -0300

    feature/strapi-aboutus-connection connected with strapi

--- client/src/Views/AboutUs/AboutUs.jsx
+++ client/src/Views/AboutUs/AboutUs.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react';
 import './AboutUs.css';
-import aboutus from '../../assets/aboutus.png'
 
 import { useState } from 'react';
 
 import axios from 'axios';
 
 function AboutUs() {
-  const [title, setTitle] = useState()
+  const [image, setImage] = useState()
+  const [text, setText] = useState()
+  const [text2, setText2] = useState()
 
-  const getTitle = async () => {
+   const getTitle = async () => {
     const res = await axios.get("http://localhost:1337/about-us");
-    const { texto } = res.data;
-    console.log(texto)
-    setTitle(texto);
+    const { text } = res.data;
+    console.log(text);
+    setText(Paragraph1);
+    setText2(Paragraph2);
+    setImage(About_us_photo)
   };
 
   useEffect (() => {
@@ -22,12 +25,12 @@ function AboutUs() {
   return (
     <div className="AboutUsPage">
      <div className="AboutUsContent">
-       <div className="AboutUsImg"><img src={aboutus} alt="Sobre nós" widht="445px" height="450px"/> </div>    
+       <div className="AboutUsImg"><img src={image} alt="Sobre nós" widht="445px" height="450px"/> </div>    
        <div className="AboutUsText" >
          <div className="AboutUsTitle"><p> Ecossistema Internacional de empresa Juniores</p></div>
          <div className="AboutUsp"></div>     
-           <div className="AboutUsp1"><p>Somos guiados pelo propósito e pelo sonho de um estado de São Paulo mais empreendedor. Por isso, temos consciência da nossa responsabilidade para fazer a diferença na vida de pequenas, médias e grandes empresas.</p></div>
-           <div className="AboutUsp2"><p>Somos guiados pelo propósito e pelo sonho de um estado de São Paulo mais empreendedor. Por isso, temos consciência da nossa responsabilidade para fazer a diferença na vida de pequenas, médias e grandes empresas.</p></div>
+           <div className="AboutUsp1"><p>{text}</p></div>
+           <div className="AboutUsp2"><p>{text2}</p></div>
        </div>
      </div>
     </div>
