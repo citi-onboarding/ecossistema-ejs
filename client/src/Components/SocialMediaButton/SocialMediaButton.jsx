d0f9baa4d496a3ca30d94c2120ac6d3eb59445a3
JoaoSouza9 <joaovictor.souza98@yahoo.com.br>
2021-12-09 18:30:08 -0300

    fix(conecte-se): logo redirecting to a new page

--- client/src/Components/SocialMediaButton/SocialMediaButton.jsx
+++ client/src/Components/SocialMediaButton/SocialMediaButton.jsx
@@ -4,7 +4,7 @@ import './SocialMediaButton.css'
 const SocialMediaButton = (props) => {
     return (
         <div class="redesocial">
-            <a href={props.link}>
+            <a href={props.link} target="_blank" rel="noreferrer">
                 <img src={props.image} alt={props.imagedescription}/>
             </a>
         </div>
